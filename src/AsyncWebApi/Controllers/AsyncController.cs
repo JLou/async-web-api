29ba345bb2ff7caecd2125e484b9e65821c59c49
Jean-Lou PIERME <jeanlou.pierme@gmail.com>
2019-04-30 15:01:39 +0200

    initial commit

---
+++ src/AsyncWebApi/Controllers/AsyncController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    public class AsyncController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet("api/async")]
+        public async Task<ActionResult<IEnumerable<string>>> GetAsync()
+        {
+            await Task.Delay(2000);
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("api/sync")]
+        public ActionResult<string> Get()
+        {
+            Task.Delay(2000).Wait();
+            return "value";
+        }
+
+        [HttpGet("api/test")]
+        public ActionResult<string> Get2()
+        {
+            FireAndForget();
+            return "toto";
+        }
+
+        private async void FireAndForget()
+        {
+            await Task.Delay(2000);
+            throw new Exception();
+        }
+    }
+}
